c34574d1c14d74ae83b3a0a6126dd951987f7c26
VictorY <6767404@gmail.com>
2018-02-26 23:13:42 +0300

    commit

---
+++ 111111/lesson01_01.cpp
@@ -0,0 +1,13 @@
+// Первая программа на C++ — урок 1 https://code-live.ru/post/cpp-hello-world/
+// C++ с нуля https://code-live.ru/tag/cpp-manual/
+
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+int main() 
+{ 
+    cout << "Hello, world!" << endl;
+    system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0; 
+}
---
+++ 111111/lesson01_02.cpp
@@ -0,0 +1,13 @@
+// C++ shell http://cpp.sh/
+// Example program
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
